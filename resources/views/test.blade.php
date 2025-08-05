0f6b3a3b1a5de0ed17e82a8764d03ca53e8034d0
unknown <izharbaloch570@gmail.com>
2025-08-05 11:03:01 +0500

    pendind task

---
+++ resources/views/test.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Test Page</h1>
+    <p>This is a test page to demonstrate Blade templating in Laravel.</p>
+
+    <p>Current Date and Time: {{ now() }}</p>
+
+    <ul>
+        @foreach($items as $item)
+            <li>{{ $item }}</li>
+        @endforeach
+    </ul>
+
+    <p>Thank you for visiting!</p>
+</body>
+</html>
