6b1e3aab6eabdecc37530ff6e69370fb9493698e
unknown <izharbaloch570@gmail.com>
2026-04-19 10:19:56 +0500

    multiple changes and remove unuse file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,6 @@
 @extends('layouts.app')
 @section('content')
     <div>
-        <h1>Welcome Blade – Day 3 {!! $name !!}</h1>
+        <h1>Welcome Blade</h1>
     </div>
-
-    <form action="{{ route('submitForm') }}" method="POST">
-        @csrf
-        @if (session('success'))
-            <div style="color: green;">
-                {{ session('success') }}
-            </div>
-        @else
-            <div style="color: red;">
-                {{ session('error') }}
-            </div>
-        @endif
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" id="name" name="name" value="{{ old('name') }}" required>
-            @error('name')
-                <div style="color: red;">{{ $message }}</div>
-            @enderror
-        </div>
-        <button type="submit">Submit</button>
-    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,65 +1,8 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class, 'index']);
-Route::post('/form-submit', [WelcomeController::class, 'submitForm'])->name('submitForm');
-
-
-// 🔍 Refined (Expert version – yaad rakhna)
-
-// Closure route → chhoti cheezon / quick testing ke liye
-
-// Controller → jab:
-
-// logic zyada ho
-
-// code reusable ho
-
-// project grow kar raha ho
-
-// 👉 Rule of thumb:
-
-// Route = traffic police 🚦
-// Controller = brain 🧠
-
-// day 2
-
-// closure route
-// Route::get('/welcom', function () {
-//     return "Welcom for day 1";
-// });
-
-// controller route
-// Route::get('/welcome', [WelcomeController::class, 'index']);
-
-
-
-// 🟢 Step 2: Route with Parameter
-Route::get('/user/{name}', function ($name) {
-    return "hello" . $name;
-});
-
-// 🟢 Step 3: Optional Parameter
-Route::get('/user/{name?}', function ($name = "Defaul Name") {
-    return "My name is:" . $name;
-});
-
-// 🟢 Step 4: Named Route
-Route::get('/dashboard', function () {
-    return 'Welcome to Dashboard';
-})->name('dashboard');
-
-Route::get('/go-to-dashboard', function () {
-    return redirect()->route('dashboard');
-});
-
-// 🟢 Step 5: POST Route with Form
-Route::get('/form', function () {
-    return view('form');
-});
-Route::post('/submit', function (Request $request) {
-    return "name:". $request->username;
+Route::get('/welcome', function(){
+    return view('welcome');
 });
